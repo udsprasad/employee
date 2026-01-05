2f0ab12e97976e25b9aef093e82139e151a9d42e
udsprasad <dspuppuluri@gmail.com>
2026-01-05 17:51:09 +0530

    optmised AccoliteProblems

--- src/main/java/com/example/employee/interviewproblems/AccoliteProblem2.java
+++ src/main/java/com/example/employee/interviewproblems/AccoliteProblem2.java
@@ -4,22 +4,27 @@ import java.util.Arrays;
 
 public class AccoliteProblem2 {
     public static void main(String[] args) {
-        Integer[] a = {0,1,2,0,1,2,0,1,2};// answer : {0,0,0,2,2,2,1,1,1}
-        int countForZero =0;
-        for(int i=0;i<a.length;i++){
-            if(a[i]==0){
-                int temp = a[i];
-                a[i]=a[countForZero];
-                a[countForZero++]=temp;
-            }
-        }
-        for(int i=a.length-1;i>countForZero;i--){
-            if(a[i]==2){
-                int temp = a[i];
-                a[i]=a[countForZero];
-                a[countForZero++]=temp;
+        int[] a = {0,1,2,0,1,2,0,1,2};// answer : {0,0,0,2,2,2,1,1,1}
+        int start =0, mid =0, end = a.length-1;
+        while(mid<=end) {
+            if(a[mid] == 0) {
+                swap(a, start, mid);
+                start++;
+                mid++;
+            } else if(a[mid] == 1) {
+                swap(a, mid, end);
+                end--;
+            } else {
+                mid++;
             }
         }
         System.out.println(Arrays.toString(a));
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+
     }
 }
--- src/main/java/com/example/employee/interviewproblems/AccoliteProblem3.java
+++ src/main/java/com/example/employee/interviewproblems/AccoliteProblem3.java
@@ -9,26 +9,25 @@ package com.example.employee.interviewproblems;
 public class AccoliteProblem3 {
     public static void main(String[] args){
         int[][] a ={{4,2,3,1},{5,6,7,8},{9,10,11,12},{16,14,15,13}};
-
+        // to print two dimensional array we need to use deepToString
         for(int i=0;i<a.length;i++){
             int min =a[i][0];
+            int minColumnIndex = 0;
             for(int j=0;j<a[0].length;j++){
                 if(min>a[i][j]){
                     min=a[i][j];
+                    minColumnIndex = j;
                 }
             }
             int max = min;
-            if(i<a.length-1) {
-                for (int j = 0; j < a.length; j++) {
-                    if (max < a[j][i]) {
-                        max = a[j][i];
-
+            for (int j = 0; j < a.length; j++) {
+                    if (max < a[j][minColumnIndex]) {
+                        max = a[j][minColumnIndex];
+                        break;
                     }
-                }
             }
             if(min==max){
                 System.out.println(min);
-                break;
             }
         }
     }
