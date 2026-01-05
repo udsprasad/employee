00c2161b7000f70f8905c36e8693567a17a618f1
udsprasad <dspuppuluri@gmail.com>
2026-01-05 16:40:13 +0530

    updated spring

--- src/main/java/com/example/employee/interviewproblems/Accolite1.java
+++ src/main/java/com/example/employee/interviewproblems/Accolite1.java
@@ -6,9 +6,24 @@ import java.util.stream.Collectors;
 // find the number which is starting with 1
 public class Accolite1 {
     public static void main(String[] args) {
-      Integer[] a = {1,2,4,101,201,401,1000};
-        List<Integer> list = Arrays.stream(a).filter(i->
-            String.valueOf(i).startsWith("1")).collect(Collectors.toList());
-        System.out.println(list);
+      int[] a = {1,2,4,101,201,401,1000};
+      optimised(a);
+
+      // using string with streams
+        Arrays.stream(a).filter(i -> String.valueOf(i).startsWith("1"))
+                .forEach(System.out::println);
+
+    }
+
+    private static void optimised(int[] a) {
+        for(int i : a) {
+            int temp = i;
+            while(temp>=10) {
+                temp/=10;
+            }
+            if(temp == 1) {
+                System.out.println(i);
+            }
+        }
     }
 }
